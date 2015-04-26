a9f9c157bb570c2dcf969e31b93f86375d98f794
Bryan Finlayson <bdfinlayson@gmail.com>
2015-04-26 11:20:14 -0500

    remove unneeded test suite dependencies

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,11 +2,8 @@ require 'rubygems'
 require 'bundler/setup'
 require 'minitest/reporters'
 require 'minitest/autorun'
-require 'minitest/ansi'
 
 reporter_options = { color: true }
 
-Minitest::Reporters.use! 
+Minitest::Reporters.use!
 [Minitest::Reporters::DefaultReporter.new(reporter_options)]
-
-
